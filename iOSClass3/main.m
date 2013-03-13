8b34b710881ca3fbf057dd3e470f568f693469e5
Sean Riggle <seanriggle@Seans-MacBook-Air.local>
2013-03-13 18:56:14 -0400

    Initial Commit

---
+++ iOSClass3/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  iOSClass3
+//
+//  Created by Sean Riggle on 3/13/13.
+//  Copyright (c) 2013 Sean Riggle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
